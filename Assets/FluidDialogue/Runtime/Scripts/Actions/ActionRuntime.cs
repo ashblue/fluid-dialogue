fb47f68249a9727fc795128c2dde88e411cccd64
Ash Blue <ash@blueashes.com>
2019-06-20 12:02:12 -0600

    Finished remaining action hooks

---
+++ Assets/FluidDialogue/Runtime/Scripts/Actions/ActionRuntime.cs
@@ -0,0 +1,66 @@
+using System;
+
+namespace CleverCrow.Fluid.Dialogues.Actions {
+    public class ActionRuntime : IAction {
+        private bool _initUsed;
+        private bool _startUsed;
+        private bool _resetReady;
+        private bool _active;
+
+        public Action OnInit { private get; set; }
+        public Action OnStart { private get; set; }
+        public Func<ActionStatus> OnUpdate { private get; set; }
+        public Action OnExit { private get; set; }
+        public Action OnReset { private get; set; }
+
+        public ActionStatus Tick () {
+            Reset();
+            Init();
+            Start();
+
+            var status = Update();
+            if (status == ActionStatus.Success) {
+                Exit();
+            }
+
+            return status;
+        }
+
+        public void End () {
+            if (_active) Exit();
+        }
+
+        private void Init () {
+            if (_initUsed) return;
+            _initUsed = true;
+
+            OnInit?.Invoke();
+        }
+
+        private void Start () {
+            if (_startUsed) return;
+            _startUsed = true;
+            _active = true;
+
+            OnStart?.Invoke();
+        }
+
+        private void Exit () {
+            _startUsed = false;
+            _resetReady = true;
+            _active = false;
+
+            OnExit?.Invoke();
+        }
+
+        private ActionStatus Update () {
+            return OnUpdate();
+        }
+
+        private void Reset () {
+            if (!_resetReady) return;
+            _resetReady = false;
+            OnReset?.Invoke();
+        }
+    }
+}
--- Assets/FluidDialogue/Tests/Editor/ActionTest.cs
+++ Assets/FluidDialogue/Tests/Editor/ActionTest.cs
@@ -1,77 +1,184 @@
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace CleverCrow.Fluid.Dialogues.Actions {
     public class ActionTest {
-        private ActionExample _action;
-
-        private class ActionExample : ActionBase {
-            public int startCount;
-            public int initCount;
-
-            protected override void OnInit () {
-                initCount += 1;
-            }
-
-            protected override void OnStart () {
-                startCount += 1;
-            }
-        }
+        private ActionRuntime _action;
 
         [SetUp]
         public void BeforeEach () {
-            _action = new ActionExample();
+            _action = new ActionRuntime {
+                OnUpdate = () => ActionStatus.Continue
+            };
         }
 
         public class TickMethod {
-            public void It_should_trigger_init () {
+            public class OnInitTriggering : ActionTest {
+                [Test]
+                public void It_should_trigger_OnInit () {
+                    var initCount = 0;
+                    _action.OnInit = () => initCount += 1;
 
+                    _action.Tick();
+
+                    Assert.AreEqual(1, initCount);
+                }
+
+                [Test]
+                public void It_should_trigger_OnInit_only_once () {
+                    var initCount = 0;
+                    _action.OnInit = () => initCount += 1;
+
+                    _action.Tick();
+                    _action.Tick();
+
+                    Assert.AreEqual(1, initCount);
+                }
             }
 
-            public void It_should_trigger_start () {
+            public class OnStartTriggering : ActionTest {
+                private int _startCount;
 
-            }
-        }
+                [SetUp]
+                public void BeforeEachMethod () {
+                    _startCount = 0;
+                    _action.OnStart = () => _startCount += 1;
+                }
 
-        public class InitMethod : ActionTest {
-            [Test]
-            public void It_should_trigger_init () {
-                _action.Init();
+                [Test]
+                public void It_should_trigger_OnStart () {
+                    _action.Tick();
 
-                Assert.AreEqual(1, _action.initCount);
-            }
+                    Assert.AreEqual(1, _startCount);
+                }
 
-            [Test]
-            public void It_should_trigger_init_only_once () {
-                _action.Init();
-                _action.Init();
+                [Test]
+                public void It_should_not_trigger_OnStart_again () {
+                    _action.Tick();
+                    _action.Tick();
 
-                Assert.AreEqual(1, _action.initCount);
-            }
-        }
+                    Assert.AreEqual(1, _startCount);
+                }
 
-        public class StartMethod : ActionTest {
-            [Test]
-            public void It_should_trigger_start () {
-                _action.Start();
+                [Test]
+                public void It_should_trigger_OnStart_after_OnUpdate_returns_success () {
+                    _action.Tick();
+                    _action.OnUpdate = () => ActionStatus.Success;
+                    _action.Tick();
+                    _action.Tick();
 
-                Assert.AreEqual(1, _action.startCount);
-            }
-
-            [Test]
-            public void It_should_not_trigger_start_again () {
-                _action.Start();
-                _action.Start();
-
-                Assert.AreEqual(1, _action.startCount);
+                    Assert.AreEqual(2, _startCount);
+                }
             }
 
-            [Test]
-            public void It_should_trigger_start_after_exit_is_called () {
-                _action.Start();
-                _action.Exit();
-                _action.Start();
+            public class OnUpdateTriggering : ActionTest {
+                [Test]
+                public void It_should_trigger_OnUpdate () {
+                    var updateCount = 0;
+                    _action.OnUpdate = () => {
+                        updateCount += 1;
+                        return ActionStatus.Success;
+                    };
 
-                Assert.AreEqual(2, _action.startCount);
+                    _action.Tick();
+
+                    Assert.AreEqual(1, updateCount);
+                }
+
+                [Test]
+                public void It_should_return_the_update_status () {
+                    _action.OnUpdate = () => ActionStatus.Continue;
+
+                    var status = _action.Tick();
+
+                    Assert.AreEqual(ActionStatus.Continue, status);
+                }
+            }
+
+            public class OnExitTriggering : ActionTest {
+                [Test]
+                public void It_should_trigger_OnExit_if_OnUpdate_returns_success () {
+                    var exitCount = 0;
+                    _action.OnUpdate = () => ActionStatus.Success;
+                    _action.OnExit = () => exitCount += 1;
+
+                    _action.Tick();
+
+                    Assert.AreEqual(1, exitCount);
+                }
+
+                [Test]
+                public void It_should_not_trigger_OnExit_if_OnUpdate_returns_continue () {
+                    var exitCount = 0;
+                    _action.OnUpdate = () => ActionStatus.Continue;
+                    _action.OnExit = () => exitCount += 1;
+
+                    _action.Tick();
+
+                    Assert.AreEqual(0, exitCount);
+                }
+            }
+
+            public class OnResetTriggering : ActionTest {
+                [Test]
+                public void It_should_trigger_reset_after_OnUpdate_returns_success () {
+                    var resetCount = 0;
+                    _action.OnReset = () => resetCount += 1;
+                    _action.OnUpdate = () => ActionStatus.Success;
+
+                    _action.Tick();
+                    _action.Tick();
+
+                    Assert.AreEqual(1, resetCount);
+                }
+
+                [Test]
+                public void It_should_not_trigger_reset_after_OnUpdate_returns_continue () {
+                    var resetCount = 0;
+                    _action.OnReset = () => resetCount += 1;
+                    _action.OnUpdate = () => ActionStatus.Continue;
+
+                    _action.Tick();
+                    _action.Tick();
+
+                    Assert.AreEqual(0, resetCount);
+                }
+            }
+
+            public class EndMethod : ActionTest {
+                [Test]
+                public void It_should_not_call_OnExit_if_Tick_returns_success () {
+                    var exitCount = 0;
+                    _action.OnUpdate = () => ActionStatus.Success;
+
+                    _action.Tick();
+                    _action.OnExit = () => exitCount += 1;
+                    _action.End();
+
+                    Assert.AreEqual(0, exitCount);
+                }
+
+                [Test]
+                public void It_should_call_OnExit_if_Tick_returned_continue () {
+                    var exitCount = 0;
+                    _action.OnUpdate = () => ActionStatus.Continue;
+
+                    _action.Tick();
+                    _action.OnExit = () => exitCount += 1;
+                    _action.End();
+
+                    Assert.AreEqual(1, exitCount);
+                }
+
+                [Test]
+                public void It_should_not_call_OnExit_if_Tick_has_not_been_called () {
+                    var exitCount = 0;
+                    _action.OnExit = () => exitCount += 1;
+
+                    _action.End();
+
+                    Assert.AreEqual(0, exitCount);
+                }
             }
         }
     }
