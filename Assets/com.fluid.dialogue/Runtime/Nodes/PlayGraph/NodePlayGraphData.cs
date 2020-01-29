ea1a23a49a6778fb1a693f90a78af5b6646c4cce
Ash Blue <ash@blueashes.com>
2020-01-29 11:21:23 -0700

    feat(graph nodes): nested graph playback now available
    Add a nested graph reference and it will play until complete. Then automatically return to the
    parent graph that played it by running the out connection (if any)

--- Assets/com.fluid.dialogue/Runtime/Nodes/PlayGraph/NodePlayGraphData.cs
+++ Assets/com.fluid.dialogue/Runtime/Nodes/PlayGraph/NodePlayGraphData.cs
@@ -3,8 +3,11 @@ using CleverCrow.Fluid.Dialogues.Graphs;
 using CleverCrow.Fluid.Dialogues.Nodes.PlayGraph;
 
 namespace CleverCrow.Fluid.Dialogues.Nodes {
+    [CreateMenu("Play Graph")]
     public class NodePlayGraphData : NodeDataBase {
         public DialogueGraph dialogueGraph;
+
+        protected override string DefaultName => "Play Graph";
 
         public override INode GetRuntime (IGraph graphRuntime, IDialogueController dialogue) {
             return new NodePlayGraph(
