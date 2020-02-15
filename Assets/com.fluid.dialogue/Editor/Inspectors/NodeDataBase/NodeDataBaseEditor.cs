bcbcb0deaf5e26da693fe969043c10d0425eb2d1
Ash Blue <ash@blueashes.com>
2020-02-15 11:24:00 -0700

    feat(spell check): dialogue and choice nodes now have spell check

--- Assets/com.fluid.dialogue/Editor/Inspectors/NodeDataBase/NodeDataBaseEditor.cs
+++ Assets/com.fluid.dialogue/Editor/Inspectors/NodeDataBase/NodeDataBaseEditor.cs
@@ -1,17 +1,26 @@
+using CleverCrow.Fluid.Dialogues.Choices;
 using CleverCrow.Fluid.Dialogues.Nodes;
+using CleverCrow.Fluid.SimpleSpellcheck;
 using UnityEditor;
+using UnityEngine;
 
 namespace CleverCrow.Fluid.Dialogues.Editors.Inspectors {
     [CustomEditor(typeof(NodeDataBase), true)]
     public class NodeDataBaseEditor : Editor {
+        private SerializedProperty _dialogue;
+        private SerializedProperty _choices;
+
         private ConditionSortableList _conditions;
         private ActionsSortableList _enterActions;
         private ActionsSortableList _exitActions;
 
         private void OnEnable () {
             var node = target as NodeDataBase;
-            _conditions = new ConditionSortableList(this, "conditions", node, node.conditions);
 
+            _dialogue = serializedObject.FindProperty("dialogue");
+            _choices = serializedObject.FindProperty("choices");
+
+            _conditions = new ConditionSortableList(this, "conditions", node, node.conditions);
             if (!node.HideInspectorActions) {
                 _enterActions = new ActionsSortableList(this, "enterActions", node, node.enterActions);
                 _exitActions = new ActionsSortableList(this, "exitActions", node, node.exitActions);
@@ -24,6 +33,27 @@ namespace CleverCrow.Fluid.Dialogues.Editors.Inspectors {
             _conditions.Update();
             _enterActions?.Update();
             _exitActions?.Update();
+
+            SpellCheckText();
+        }
+
+        private void SpellCheckText () {
+            if ((_dialogue != null || _choices != null) && GUILayout.Button("Spell Check")) {
+                SpellCheck.Instance.ClearValidation();
+            } else {
+                return;
+            }
+
+            if (_dialogue != null) {
+                SpellCheck.Instance.AddValidation(_dialogue.displayName, _dialogue.stringValue);
+            }
+
+            if (_choices != null) {
+                for (var i = 0; i < _choices.arraySize; i++) {
+                    var choice = _choices.GetArrayElementAtIndex(i).objectReferenceValue as ChoiceData;
+                    SpellCheck.Instance.AddValidation($"Choice {i}", choice.text);
+                }
+            }
         }
     }
 }
