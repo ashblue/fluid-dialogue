25cb3410890b43ef75ed379a688ff49eae56369d
Ash Blue <ash@clevercrowgames.com>
2022-09-19 00:32:55 -0700

    chore(oyster): upgrading to latest version 3.X

--- publish-nightly.sh
+++ publish-nightly.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 setup_git() {
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "Travis CI - Bot"
+    git config --global user.email "no-reply@github.com"
+    git config --global user.name "Github-Actions[bot]"
 }
 
 update_nightly_branch() {
     printf "Run nightly update \n"
-    git remote add origin-nightly https://${GH_TOKEN}@github.com/ashblue/fluid-dialogue.git
+    git remote add origin-nightly "https://${GH_TOKEN}@github.com/ashblue/fluid-dialogue.git"
     git subtree split --prefix Assets/com.fluid.dialogue -b nightly
     git push -f origin-nightly nightly:nightly
 }
