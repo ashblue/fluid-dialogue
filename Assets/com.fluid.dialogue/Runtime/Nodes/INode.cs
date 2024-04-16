f7fe75fd16829d1944cc365998dfe8a84470928a
Ash Blue <ash@clevercrowgames.com>
2024-04-16 13:03:28 -0700

    refactor(conditions): definition data is now exposed to implement custom runtime choice displays

--- Assets/com.fluid.dialogue/Runtime/Nodes/INode.cs
+++ Assets/com.fluid.dialogue/Runtime/Nodes/INode.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using CleverCrow.Fluid.Dialogues.Actions;
 using CleverCrow.Fluid.Dialogues.Choices;
+using CleverCrow.Fluid.Dialogues.Conditions;
 
 namespace CleverCrow.Fluid.Dialogues.Nodes {
     public interface INode : IUniqueId {
@@ -8,6 +9,7 @@ namespace CleverCrow.Fluid.Dialogues.Nodes {
         List<IAction> ExitActions { get; }
         bool IsValid { get; }
         List<IChoice> HubChoices { get; }
+        IReadOnlyList<ICondition> Conditions { get; }
 
         /// <summary>
         /// Returns the first valid child node
